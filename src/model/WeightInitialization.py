b06c2bfedfdbf0fc1f6e6d037ed9cdab53dfa54f
Rafly Hanggaraksa <115408951+raflyhangga@users.noreply.github.com>
2025-03-11 13:00:55 +0700

    nit: commentz

--- src/model/WeightInitialization.py
+++ src/model/WeightInitialization.py
@@ -1,8 +1,8 @@
 """
 Cara penggunaan:
 initiator = WeightInitiatior(init_method=WeightInitMethod.NORMAL, neurons=2, layers=3, mean=24, std=2, seed=69)
-initiator = WeightInitiatior(WeightInitMethod.UNIFORM, neurons=2, layers=3, lower_bound=4, upper_bound=7)
-initiator = WeightInitiatior(WeightInitMethod.ZERO, neurons=2, layers=3)
+initiator = WeightInitiatior(init_method=WeightInitMethod.UNIFORM, neurons=2, layers=3, lower_bound=4, upper_bound=7)
+initiator = WeightInitiatior(init_method=WeightInitMethod.ZERO, neurons=2, layers=3)
 weights,bias = initiator.get_weights()
 """
 
